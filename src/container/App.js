0e11d50472752b2d14955296374610e4a168eab0
Aric Hylton <arich212@gmail.com>
2019-05-18 22:29:59 -0500

    added actions and reducers for searchBox

--- src/container/App.js
+++ src/container/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../SearchBox';
+import Scroll from '../Scroll';
 import './App.css';
 
 class App extends React.Component {
